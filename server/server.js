bbb619e618e7bda8a875eca456b5f26810757f07
EKshawon <shawon22205101878@diu.edu.bd>
2025-12-09 09:58:03 +0600

    Final Commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
+import imageRouter from "./routes/imageRoutes.js";
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+await  connectDB();
+
+
+app.use('/api/user', userRouter);
+app.use("/api/image", imageRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to database:", err);
+    process.exit(1);
+  });
