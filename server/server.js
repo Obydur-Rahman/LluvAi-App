abd335e2684839d4bf532b85774057d46487f9a2
sohel4444 <sohelrana97cse@gmail.com>
2025-12-09 09:32:21 +0600

    Final commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/user', userRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to database:", err);
+    process.exit(1);
+  });
